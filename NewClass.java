2b889ffe87aa6c3d0003bbbf660913e59420e316
garik <imartynkin@yandex.ru>
2024-01-29 15:03:42 +0900

    second commit

---
+++ NewClass.java
@@ -0,0 +1,15 @@
+public class NewClass {
+    private String a = "a";
+
+    public NewClass(String a) {
+        this.a = a;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
